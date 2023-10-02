42f452d19c398db9e46c7e1b8a3bf2c12203fc44
Samudra Zulqifli <samudrazulqifli24@gmail.com>
2023-10-02 12:10:28 +0700

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,15 @@ import NavbarMenu from "./Navbar";
 
 const Header = () => {
   return (
-    <section className="bg-gradient-to-r from-[#5B68DF] to-[#00FFD1] w-full h-[189px] md:h-[682px]">
+    <section className="bg-gradient-to-r from-[#5B68DF] to-[#00FFD1] w-full h-[189px] md:h-[396px] lg:h-[682px]">
       <div className="relative">
         <NavbarMenu></NavbarMenu>
       </div>
       <div className="text-white pt-8">
-        <div className="flex text-center justify-center px-[58px] w-full mt-4 text-[20.59px] md:text-[70px] md:mt-24 md:px-[160px] font-bold md:font-semibold">
+        <div className="flex text-center justify-center px-[58px] w-full mt-5 text-[20.59px] md:text-[50px] lg:text-[70px] lg:mt-24 lg:px-[160px] font-bold lg:font-semibold">
           Create Powerful Content That Matters
         </div>
-        <div className="w-full text-center flex justify-center font-normal mt-[12px] text-[8.32px] px-[80px] md:text-[22px] md:mt-[50.07px]">
+        <div className="w-full text-center flex justify-center font-normal mt-[12px] text-[8.32px] px-[80px] md:text-[15px] md:px-[180px] md:mt-[34px] lg:text-[22px] lg:mt-[50.07px]">
           <p>
             Discover how to establish strong connections with your target
             audience by utilizing our complimentary resources and content
